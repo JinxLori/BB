d9237f351dce5a25349baebd945c659521048710
JinxLori <2248865931@qq.com>
2021-02-01 16:17:01 +0800

    Android support 与AndroidX版本冲突解决，全部用AndroidX的库；滑动卡片引用成功

--- app/src/main/java/com/android/loginui/androidsidebar/Style4Activity.java
+++ app/src/main/java/com/android/loginui/androidsidebar/Style4Activity.java
@@ -2,15 +2,15 @@ package com.android.loginui.androidsidebar;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
@@ -24,7 +24,6 @@ import com.android.loginui.other.MyViewPagerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-import me.majiajie.pagerbottomtabstrip.MaterialMode;
 import me.majiajie.pagerbottomtabstrip.NavigationController;
 import me.majiajie.pagerbottomtabstrip.PageNavigationView;
 import me.majiajie.pagerbottomtabstrip.listener.OnTabItemSelectedListener;
--- app/src/main/java/com/android/loginui/fragment/FragmentOne.java
+++ app/src/main/java/com/android/loginui/fragment/FragmentOne.java
@@ -1,22 +1,35 @@
 package com.android.loginui.fragment;
 
-import android.content.Intent;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import androidx.annotation.Nullable;
+
+import com.android.loginui.fragment.One.CardData;
+import com.android.loginui.fragment.One.CommentArrayAdapter;
+import com.android.loginui.fragment.One.ExampleDataset;
+import com.android.loginui.fragment.One.ItemsCountView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.loginui.MainActivity;
 import com.android.loginui.R;
-import com.android.loginui.fragment.One.AddScheduleFragment;
+import com.ramotion.expandingcollection.ECBackgroundSwitcherView;
+import com.ramotion.expandingcollection.ECCardData;
+import com.ramotion.expandingcollection.ECPagerView;
+import com.ramotion.expandingcollection.ECPagerViewAdapter;
 
 public class FragmentOne extends Fragment {
+    private ECPagerView ecPagerView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -31,6 +44,73 @@ public class FragmentOne extends Fragment {
                 Toast.makeText(getActivity(),"You clicked float button",Toast.LENGTH_SHORT).show();
             }
         });
+
+        // Create adapter for pager
+        ECPagerViewAdapter adapter = new ECPagerViewAdapter(getContext(), new ExampleDataset().getDataset()) {
+            @Override
+            public void instantiateCard(LayoutInflater inflaterService, ViewGroup head, ListView list, final ECCardData data) {
+                final CardData cardData = (CardData) data;
+
+                // Create adapter for list inside a card and set adapter to card content
+                CommentArrayAdapter commentArrayAdapter = new CommentArrayAdapter(getContext(), cardData.getListItems());
+                list.setAdapter(commentArrayAdapter);
+                list.setDivider(getResources().getDrawable(R.drawable.list_divider));
+                list.setDividerHeight((int) pxFromDp(getContext(), 0.5f));
+                list.setSelector(R.color.transparent);
+                list.setBackgroundColor(Color.WHITE);
+                list.setCacheColorHint(Color.TRANSPARENT);
+
+                // Add gradient to root header view
+                View gradient = new View(getContext());
+                gradient.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, AbsListView.LayoutParams.MATCH_PARENT));
+                gradient.setBackgroundDrawable(getResources().getDrawable(R.drawable.card_head_gradient));
+                head.addView(gradient);
+
+                // Inflate main header layout and attach it to header root view
+                inflaterService.inflate(R.layout.simple_head, head);
+
+                // Set header data from data object
+                TextView title = (TextView) head.findViewById(R.id.title);
+                title.setText(cardData.getHeadTitle());
+                ImageView avatar = (ImageView) head.findViewById(R.id.avatar);
+                avatar.setImageResource(cardData.getPersonPictureResource());
+                TextView name = (TextView) head.findViewById(R.id.name);
+                name.setText(cardData.getPersonName() + ":");
+                TextView message = (TextView) head.findViewById(R.id.message);
+                message.setText(cardData.getPersonMessage());
+                TextView viewsCount = (TextView) head.findViewById(R.id.socialViewsCount);
+                viewsCount.setText(" " + cardData.getPersonViewsCount());
+                TextView likesCount = (TextView) head.findViewById(R.id.socialLikesCount);
+                likesCount.setText(" " + cardData.getPersonLikesCount());
+                TextView commentsCount = (TextView) head.findViewById(R.id.socialCommentsCount);
+                commentsCount.setText(" " + cardData.getPersonCommentsCount());
+
+                // Add onclick listener to card header for toggle card state
+                head.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(final View v) {
+                        ecPagerView.toggle();
+                    }
+                });
+            }
+        };
+
+        ecPagerView = (ECPagerView) view.findViewById(R.id.ec_pager_element);
+
+        ecPagerView.setPagerViewAdapter(adapter);
+        ecPagerView.setBackgroundSwitcherView((ECBackgroundSwitcherView) view.findViewById(R.id.ec_bg_switcher_element));
+
+        final ItemsCountView itemsCountView = (ItemsCountView) view.findViewById(R.id.items_count_view);
+        ecPagerView.setOnCardSelectedListener(new ECPagerView.OnCardSelectedListener() {
+            @Override
+            public void cardSelected(int newPosition, int oldPosition, int totalElements) {
+                itemsCountView.update(newPosition, oldPosition, totalElements);
+            }
+        });
         return view;
+    }
+
+    public static float pxFromDp(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
     }
 }
